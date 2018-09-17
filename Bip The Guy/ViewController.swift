9442e1e1ca86d8d15e65d1ee803e6b0da2df6bb9
Michael Burke <michaelburke@michaels-macbook-pro-2.local>
2018-09-16 23:44:29 -0400

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Michael Burke on 9/16/18.
+//  Copyright © 2018 Michael Burke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
